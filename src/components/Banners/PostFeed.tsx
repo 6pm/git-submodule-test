59c53592e1d6892a883601924603826988f0f8f6
Slava <slavik.troyan@gmail.com>
2025-03-04 08:53:25 +0100

    disable ts types

--- src/components/Banners/PostFeed.tsx
+++ src/components/Banners/PostFeed.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from "react";
 
 // Mock API call to fetch posts
--- src/components/Banners/index.tsx
+++ src/components/Banners/index.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from "react";
 import ClientComponent from "./ClientComponent";
 import PostFeed from "./PostFeed";
